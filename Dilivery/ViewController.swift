6518de422d9ab2fe6040d893718476b554d86cb4
sanhan42 <sangmin613@gmail.com>
2022-07-08 09:41:09 +0900

    first commit

--- Dilivery/ViewController.swift
+++ Dilivery/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var bannerImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
